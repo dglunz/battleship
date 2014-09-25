377f9814dd500beb2a2ac1497563ec8fcba0dfa4
Daniel Glunz <dannyglunz@gmail.com>
2014-09-24 22:39:08 -0600

    static placement for testing

--- lib/battleship-dglunz/player.rb
+++ lib/battleship-dglunz/player.rb
@@ -17,15 +17,29 @@ class Player
     end
   end
 
+  def static_ship_placement
+    fleet[0].location = 'a1 a2'
+    fleet[1].location = 'b1 b2 b3'
+  end
+
   private
 
-  def generate_random_coordinates(ship)
-    coordinates = []
-    ship.size.times do
-      coordinates << %w(a b c d).sample + ['1','2','3','4'].sample
-    end
-    coordinates.join(" ")
-  end
+
+  # def generate_random_coordinates(ship)
+  #   coordinates = []
+  #   row =  %w(a b c d)
+  #   column = %w(1 2 3 4)
+  #   flip = [1, 2].sample
+  #   coordinate_base = row.sample + column.sample
+  #   ship.size.times do |index|
+  #     if flip == 1
+  #       coordinates << coordinate_base[0] + column[column.index(coordinate_base[1])]
+  #     else
+  #       coordinates << row.sample + coordinate_base[1]
+  #     end
+  #   end
+  #   coordinates.join(" ")
+  # end
 
   def update_ship_location(ship)
     ship.location = coordinates
